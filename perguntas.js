291e3f92e36e193ebc6b432c28689a96a61b17b5
estudanteLH36 <00001103939725SP@al.educacao.sp.gov.br>
2024-11-08 11:38:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é programação?',
+    'E um metodo de tecnologia'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil e Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'o que e um jogo?',
+    'como se joga?',
+    'o jogo e uma forma de diverção você pode jogar com celulares ou video games',
+)
